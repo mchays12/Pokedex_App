c2950b54b00a667b91dab971246ee4a11967aa19
mchays12 <mchays12@gmail.com>
2023-02-06 15:08:07 -0500

    Updated List
    Made it less organized because learning??

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,15 @@
-let pokemonTypes=[
-  'grass', 
-  'electric', 
-  'artificial'
-];
-
 let pokemonList = [
   { pokemon1Height: 10,
     pokemon1Name: 'Bulbasaur',
-    pokemon1Type: pokemonTypes [0]
+    pokemon1Type: 'grass'
   },
   {pokemon2Height: 8,
     pokemon2Name: 'Pikachu',
-    pokemon2Type: pokemonTypes[1]
+    pokemon2Type: 'electric'
   },
   { pokemon3Height: 5,
     pokemon3Name: 'Mewtwo',
-    pokemon3Type: pokemonTypes[2]
+    pokemon3Type: 'artificial'
   }
 ];
 
