7bd5acab10261eefd7f19a48db004e25c6e5c1ba
mchays12 <mchays12@gmail.com>
2023-02-06 15:19:59 -0500

    Re Added Array
    Re added the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
+let pokemonTypes=[
+  'grass', 
+  'electric', 
+  'artificial'
+];
+
 let pokemonList = [
   { pokemon1Height: 10,
     pokemon1Name: 'Bulbasaur',
-    pokemon1Type: 'grass'
+    pokemonTypes
   },
   {pokemon2Height: 8,
     pokemon2Name: 'Pikachu',
-    pokemon2Type: 'electric'
+    pokemonTypes
   },
   { pokemon3Height: 5,
     pokemon3Name: 'Mewtwo',
-    pokemon3Type: 'artificial'
+    pokemonTypes
   }
 ];
 
