391677d20017fca6c86f6cfeb3752cb85a395c86
mchays12 <mchays12@gmail.com>
2023-03-06 14:26:40 -0500

    Updated Script
    Put an IIFE on my pokemon list and trying to get the function to work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,64 @@
-let pokemonList = [
-  { height: 10,
-    name: 'Bulbasaur',
-    types:[
-      'grass', 
-      'poison'
-    ]
-  },
-  { height: 8,
-    name: 'Pikachu',
-    types:[ 
-      'electric' 
-    ]
-  },
-  { height: 5,
-    name: 'Mewtwo',
-    types:[
-      'artificial'
-    ]
+let pokemonRepository = (function() {
+  let pokemonList = [
+    { height: 10,
+      name: 'Bulbasaur',
+      types:[
+        'grass', 
+        'poison'
+      ]
+    },
+    { height: 8,
+      name: 'Pikachu',
+      types:[ 
+        'electric' 
+      ]
+    },
+    { height: 5,
+      name: 'Mewtwo',
+      types:[
+        'artificial'
+      ]
+    }
+  ];
+
+function getAll () {
+  return pokemonList;
+}
+function add (pokemon) {
+  pokemonList.push(pokemon);
+}
+
+return {
+  add: add,
+  getAll: getAll
+}
+
+})();
+
+pokemonRepository.getAll();
+
+pokemonRepository.add({ name: 'Charmander'});
+
+
+
+
+pokemonRepository.forEach(function(item) {
+  if (item.height >9){
+    document.write("<p>" + item.name + " " + "(height:" + item.height + ")" + " Wow! That's big!" + "</p>")
+  }else {
+    document.write("<p>" + item.name + " " + "(height:" + item.height + ")" + "</p>");
   }
-];
+});
+
+
+
+
 /* this is going to display the name of my pokemon, a space, and their height in parenthesis
 if the height is greater than 9, it will also display the message, "wow! that's big"*/
-for (let i=0; i<pokemonList.length; i++) { 
+/* for (let i=0; i<pokemonList.length; i++) { 
   if (pokemonList[i].height >9){
   document.write("<p>" + pokemonList[i].name + " " + "(height:" + pokemonList[i].height + ")" + " Wow! That's big!" + "</p>")
   }else {
   document.write("<p>" + pokemonList[i].name + " " + "(height:" + pokemonList[i].height + ")" + "</p>");
   }
-}
+} */
